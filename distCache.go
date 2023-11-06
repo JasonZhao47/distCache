05e9d2f08ce13637e50235531eb6e17718a95b9e
-=[ Heisenberg ]=- <roastedcookie47@hotmail.com>
2023-11-06 20:47:02 +0800

    feature: distribution cache

--- distCache.go
+++ distCache.go
@@ -1,11 +1,83 @@
 package distCache
 
 import (
+	"errors"
 	"github.com/jasonzhao47/distCache/internal/lru"
 	"sync"
 )
 
-// question: how to test concurrency?
+type GroupCache struct {
+	getter func(byte) ([]byte, error)
+	// getter func - callback function for data source
+	// get the real data out from it
+	name  string
+	cache *cache[byte, []byte]
+}
+
+var (
+	ErrInternalError = errors.New("内部错误")
+	// global variable - shared memory!
+	groups map[string]*GroupCache
+	mu     sync.RWMutex
+)
+
+// DistCache question: how to test concurrency?
+type DistCache interface {
+	GetGroup(name string) (*GroupCache, error)
+	Get(name string, key byte) ([]byte, error)
+}
+
+func NewGroups(getter func(byte) ([]byte, error),
+	name string, cacheBytes int) DistCache {
+	if getter == nil {
+		panic("empty data getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	gc := &GroupCache{
+		getter: getter,
+		name:   name,
+		cache:  &cache[byte, []byte]{cacheBytes: cacheBytes},
+	}
+	groups[name] = gc
+	return gc
+}
+
+func (c *GroupCache) GetGroup(name string) (*GroupCache, error) {
+	mu.RLock()
+	defer mu.RUnlock()
+	group, ok := groups[name]
+	if !ok {
+		return nil, ErrInternalError
+	}
+	return group, nil
+}
+
+func (c *GroupCache) Get(name string, key byte) ([]byte, error) {
+	if c.cache == nil {
+		return nil, ErrInternalError
+	}
+	g, err := c.GetGroup(name)
+	if err != nil {
+		return []byte{}, err
+	}
+	val, ok := g.cache.get(key)
+	if !ok {
+		// val, peerOk := GetFromPeer()
+		// if !peerOk
+		data, err := c.getter(key)
+		if err != nil {
+			return nil, err
+		}
+		c.cache.add(key, data)
+	}
+	// 1. found in cache
+	// 2. not found in cache
+	// a. get from peer
+	// b. not found - get from source
+	// i set cache
+	return val, nil
+}
 
 // cache: wrapper for LRUCache, thread safe
 type cache[T comparable, V interface{}] struct {
